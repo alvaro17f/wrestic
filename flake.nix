e4b4dce9633486016bbbc700822f0e17edda344e
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-02-07 19:57:02 +0100

    chore: update

--- flake.nix
+++ flake.nix
@@ -5,29 +5,30 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }:
-  let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
-    lib = pkgs.lib;
-  in
-  {
-    defaultPackage.${pkgs.system} = pkgs.rustPlatform.buildRustPackage  {
-      name = "wrestic";
-      src = self;
-      cargoHash = "sha256-XKhFey2QimyiVuhF0L61Quieaa+FtKoeu5tgKNvS0Ms=";
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+      lib = pkgs.lib;
+    in
+    {
+      defaultPackage.${pkgs.system} = pkgs.rustPlatform.buildRustPackage {
+        name = "wrestic";
+        src = self;
+        useFetchCargoVendor = true;
+        cargoHash = "sha256-QHCGUL2puzBShuudqiq29SS0JTKElaRptgBsAqq3q80=";
 
-      meta = with lib; {
-        homepage = "https://github.com/alvaro17f/wrestic";
-        description = "wrestic";
-        license = licenses.mit;
-        maintainers = with maintainers; [ alvaro17f ];
-        platforms = platforms.unix;
-        #changelog = "https://github.com/alvaro17f/wrestic/blob/${version}/CHANGELOG.md";
-        mainProgram = "wrestic";
+        meta = with lib; {
+          homepage = "https://github.com/alvaro17f/wrestic";
+          description = "wrestic";
+          license = licenses.mit;
+          maintainers = with maintainers; [ alvaro17f ];
+          platforms = platforms.unix;
+          #changelog = "https://github.com/alvaro17f/wrestic/blob/${version}/CHANGELOG.md";
+          mainProgram = "wrestic";
+        };
       };
     };
-  };
 }
-
