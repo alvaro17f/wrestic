455d2d6963ef3c11f454a00627a4e485c564c777
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-02-07 19:58:43 +0100

    refactor: improve environment variable setting

--- src/utils/set_environment_variables.rs
+++ src/utils/set_environment_variables.rs
@@ -5,7 +5,7 @@ use std::env;
 pub fn set_environment_variables(setting: &Settings) -> Result<()> {
     env::set_var("USER", &setting.user);
     env::set_var("RESTIC_PASSWORD", &setting.restic_password);
-    for env in &setting.env {
+    while let Some(env) = &setting.env {
         for (key, value) in env {
             env::set_var(key, value);
         }
